65a0f69369b13983a344e741ad741394c715767a
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2023-02-03 20:30:08 +0100

    add file words.txt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        
-    }
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner myScanner = new Scanner(new File("words.txt"));
+
+        List<String> words = new ArrayList<>();
+
+        while (myScanner.hasNext()) {
+            words.add(myScanner.nextLine());
+        }
+
+        Random myRandom = new Random();
+        String word = words.get(myRandom.nextInt(words.size()) + 1);
+        System.out.println(word);
+     }
 }
